5b21b63badc1d0d57487a29536ea88132135dec0
Dmitri Chernysh <dmitriy.chernysh@gmail.com>
2023-02-05 00:04:57 +0200

    UI state and fake people list is ready

--- feature/people_list/src/main/kotlin/com/mobiledevpro/peoplelist/view/PeopleListScreen.kt
+++ feature/people_list/src/main/kotlin/com/mobiledevpro/peoplelist/view/PeopleListScreen.kt
@@ -17,60 +17,103 @@
  */
 package com.mobiledevpro.peoplelist.view
 
-import androidx.compose.foundation.background
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.mobiledevpro.people.profile.domain.model.PeopleProfile
 import com.mobiledevpro.ui.component.ScreenBackground
 import com.mobiledevpro.ui.theme.AppTheme
 
 
+@OptIn(ExperimentalLifecycleComposeApi::class)
 @Composable
 fun PeopleListScreen(onNavigateToProfile: (profileId: Int) -> Unit) {
     val viewModel: PeopleListViewModel = viewModel()
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val context = LocalContext.current
 
     ScreenBackground(
         modifier = Modifier
             .fillMaxSize()
     ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
-                .background(color = Color(0x80EF6C00))
-        ) {
-            Text(
-                text = "People list",
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .padding(16.dp)
-                    .align(Alignment.Center),
-                style = MaterialTheme.typography.bodyLarge
 
-            )
+        when (uiState) {
+            is PeopleProfileUIState.Loading -> Loading()
+            is PeopleProfileUIState.Empty -> NoPeopleFound()
+            is PeopleProfileUIState.Success ->
+                PeopleList(list = (uiState as PeopleProfileUIState.Success).profileList)
 
-            Button(
-                onClick = { onNavigateToProfile(2) },
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .defaultMinSize(minWidth = 144.dp, minHeight = 48.dp)
-            ) {
-                Text(
-                    text = "Person profile"
-                )
+            is PeopleProfileUIState.Fail -> {
+                NoPeopleFound()
+                LaunchedEffect(true) {
+                    Toast.makeText(
+                        context,
+                        (uiState as PeopleProfileUIState.Fail).throwable.localizedMessage,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
             }
+
+        }
+
+    }
+}
+
+@Composable
+private fun NoPeopleFound() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "No people found",
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.Center),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@Composable
+private fun Loading() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Loading...", modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.Center),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@Composable
+private fun PeopleList(list: List<PeopleProfile>) {
+    LazyColumn {
+        items(list) {
+            Text(text = it.name)
         }
     }
 }
