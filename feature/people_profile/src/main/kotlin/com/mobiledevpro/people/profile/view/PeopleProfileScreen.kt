11caa07a07f512017cde11865ad27625e665e977
Dmitri Chernysh <dmitriy.chernysh@gmail.com>
2023-02-15 20:47:11 +0200

    Re-done People Profile screen without ConstraintLayout. + Fix to force LayoutInspector to work

--- feature/people_profile/src/main/kotlin/com/mobiledevpro/people/profile/view/PeopleProfileScreen.kt
+++ feature/people_profile/src/main/kotlin/com/mobiledevpro/people/profile/view/PeopleProfileScreen.kt
@@ -18,30 +18,36 @@
 package com.mobiledevpro.people.profile.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.boundsInRoot
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
 import com.mobiledevpro.people.profile.R
 import com.mobiledevpro.people.profile.domain.model.PeopleProfile
 import com.mobiledevpro.people.profile.domain.model.fakPeopleProfileList
@@ -60,61 +66,52 @@ import com.mobiledevpro.ui.R as RApp
 
 @Composable
 fun PeopleProfileScreen(
-    profile : PeopleProfile,
+    profile: PeopleProfile,
     onBackPressed: () -> Unit,
     onOpenChatWith: (profile: PeopleProfile) -> Unit
 ) {
+
+    val backgroundBoxTopOffset = remember { mutableStateOf(0) }
 
     ScreenBackground(
         modifier = Modifier
             .fillMaxSize()
     ) {
 
-        ConstraintLayout {
-            val (profilePicture, profileContent, boxTopBorder, contentBox, socialIcons, buttonHi) = createRefs()
+        //Background with rounded top-corners
+        Box(
+            modifier = Modifier
+                .offset { IntOffset(0, backgroundBoxTopOffset.value) }
+                .clip(RoundedCornerShape(topStart = 48.dp, topEnd = 48.dp))
+                .background(color = MaterialTheme.colorScheme.surfaceColorAtElevation(4.dp))
+        )
 
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top
+        ) {
             IconButton(
                 onClick = { onBackPressed() },
-                modifier = Modifier.padding(16.dp)) {
+                modifier = Modifier.padding(16.dp)
+            ) {
                 Icon(
                     painter = painterResource(id = RApp.drawable.ic_arrow_back_white_24dp),
                     contentDescription = ""
                 )
             }
 
-            //Line to link the top side of the Box to
-            Divider(
-                thickness = 1.dp,
-                color = Color.Transparent,
-                modifier = Modifier.constrainAs(boxTopBorder) {
-                    top.linkTo(profilePicture.top, margin = 48.dp)
-                    bottom.linkTo(profilePicture.bottom)
-                }
-            )
-
-            //Background with rounded top-corners
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .clip(RoundedCornerShape(topStart = 48.dp, topEnd = 48.dp))
-                    .background(color = MaterialTheme.colorScheme.surfaceColorAtElevation(4.dp))
-                    .constrainAs(contentBox) {
-                        top.linkTo(boxTopBorder.bottom)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                    }
-            )
-
             ProfilePicture(
                 photoUrl = profile.photoUrl,
                 onlineStatus = profile.status,
                 imageSize = 144.dp,
                 modifier = Modifier
-                    .constrainAs(profilePicture) {
-                        top.linkTo(parent.top)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                    }.padding(paddingValues = PaddingValues(16.dp, 64.dp, 16.dp, 16.dp))
+                    .padding(paddingValues = PaddingValues(16.dp, 16.dp, 16.dp, 16.dp))
+                    .align(Alignment.CenterHorizontally)
+                    .onGloballyPositioned {
+                        val rect = it.boundsInRoot()
+                        backgroundBoxTopOffset.value =
+                            rect.topCenter.y.toInt() + (rect.bottomCenter.y - rect.topCenter.y).toInt() / 2
+                    }
             )
 
             ProfileContent(
@@ -123,39 +120,30 @@ fun PeopleProfileScreen(
                 alignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .padding(8.dp)
-                    .constrainAs(profileContent) {
-                        top.linkTo(profilePicture.bottom)
-                        start.linkTo(profilePicture.start)
-                        end.linkTo(profilePicture.end)
-                    }
+                    .align(Alignment.CenterHorizontally)
             )
 
-            ProfileSocialIcons(
-                modifier = Modifier.constrainAs(socialIcons) {
-                    top.linkTo(profileContent.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                })
+            ProfileSocialIcons(modifier = Modifier.align(Alignment.CenterHorizontally))
 
-            Button(
-                onClick = {
-                    profile.also(onOpenChatWith)
-                },
+            Row(
                 modifier = Modifier
-                    .padding(bottom = 48.dp, top = 16.dp, start = 16.dp, end = 16.dp)
-                    .defaultMinSize(minHeight = 48.dp, minWidth = 144.dp)
-                    .constrainAs(buttonHi) {
-                        bottom.linkTo(parent.bottom)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                    },
-                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiary),
+                    .fillMaxHeight()
+                    .align(Alignment.CenterHorizontally),
+                verticalAlignment = Alignment.Bottom
             ) {
-                Text(text = "Say Hi \uD83D\uDC4B")
+                Button(
+                    onClick = {
+                        profile.also(onOpenChatWith)
+                    },
+                    modifier = Modifier
+                        .padding(bottom = 48.dp, top = 16.dp, start = 16.dp, end = 16.dp)
+                        .defaultMinSize(minHeight = 48.dp, minWidth = 144.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiary),
+                ) {
+                    Text(text = "Say Hi \uD83D\uDC4B")
+                }
             }
-
         }
-
 
     }
 }
